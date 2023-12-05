86147448745bf55db8cfa829a480bfd8b083cdb0
duccsuccer <edwardhwc06@gmail.com>
2023-12-05 14:43:03 +0800

    a

---
+++ mobileplan/mobileplan/MobilePlan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobileplan
+{
+    internal class MobilePlan
+    {
+        public string Number { get; set; }
+        public int Data { get; set; }
+        MobilePlan() { }
+        public MobilePlan(string number, int data)
+        {
+            Number = number;
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return $"Number: {Number}, Data: {Data}";
+        }   
+    }
+}
